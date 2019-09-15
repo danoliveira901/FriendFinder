9c13dbf314e47b8b21668bf283e4b568f430cd3f
danyel oliveira <oliveiradanyel901@gmail.com>
2019-09-15 17:46:46 -0600

    api routes add

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
